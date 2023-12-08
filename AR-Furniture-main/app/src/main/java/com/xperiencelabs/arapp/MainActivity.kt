d6cb74adb765748f485ba8325393ffec680b94f5
JustinR <126148603+JustinRGtr@users.noreply.github.com>
2023-12-08 07:53:10 +0700

    final
    final

--- AR-Furniture-main/app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
+++ AR-Furniture-main/app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
@@ -1,89 +1,96 @@
 package com.xperiencelabs.arapp
 
-import android.content.Context
-import android.graphics.BitmapFactory
-import android.media.MediaPlayer
-import android.net.Uri
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.view.isGone
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.ar.core.Config
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
-import io.github.sceneview.ar.node.AugmentedImageNode
-import io.github.sceneview.ar.node.PlacementMode
-import io.github.sceneview.material.setExternalTexture
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Rotation
-import io.github.sceneview.node.VideoNode
+import io.github.sceneview.ar.node.PlacementMode
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sceneView: ArSceneView
-    lateinit var placeButton: ExtendedFloatingActionButton
-    private lateinit var modelNode: ArModelNode
-    private lateinit var videoNode: VideoNode
-    private lateinit var mediaPlayer:MediaPlayer
-
+    private lateinit var placeButton: ExtendedFloatingActionButton
+    private lateinit var setButton: FloatingActionButton
+    private val modelFiles = listOf("models/desktop_computer.glb", "models/gaming_chair.glb")
+    private var modelIndex = 0
+    private var placedModelNodes = mutableListOf<ArModelNode>() // List to keep track of placed models
+    private var currentModelNode: ArModelNode? = null
+    private var lastRotation = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
-            this.lightEstimationMode = Config.LightEstimationMode.DISABLED
-        }
-
-        mediaPlayer = MediaPlayer.create(this,R.raw.ad)
-
-        placeButton = findViewById(R.id.place)
+        sceneView = findViewById(R.id.sceneView) ?: throw IllegalStateException("SceneView not found")
+        placeButton = findViewById(R.id.place) ?: throw IllegalStateException("Place button not found")
+        setButton = findViewById(R.id.set) ?: throw IllegalStateException("Set button not found")
 
         placeButton.setOnClickListener {
-            placeModel()
+            if (modelIndex < modelFiles.size) {
+                currentModelNode = loadAndPlaceModel(modelFiles[modelIndex])
+                // Do not increment modelIndex here. It will be incremented after "Set" is clicked
+            } else {
+                // Handle the case where there are no more models to place
+                showToast("All models have been placed.")
+            }
         }
 
-        videoNode = VideoNode(sceneView.engine, scaleToUnits = 0.7f, centerOrigin = Position(y=-4f), glbFileLocation = "models/plane.glb", player = mediaPlayer, onLoaded = {_,_ ->
-            mediaPlayer.start()
-        })
+        setButton.setOnClickListener {
+            currentModelNode?.let {
+                placedModelNodes.add(it)
+                currentModelNode = null // Clear the reference to allow a new model to be placed
+                modelIndex++ // Ready to place the next model
+            }
+        }
 
-        modelNode = ArModelNode(sceneView.engine,PlacementMode.INSTANT).apply {
+        sceneView.setOnTouchListener { _, event ->
+            handleRotation(event)
+            true
+        }
+    }
+
+    private fun handleRotation(event: MotionEvent) {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                lastRotation = event.x
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (!placedModelNodes.isEmpty()) {
+                    val rotationDelta = (event.x - lastRotation) / 10
+                    val currentRotation = placedModelNodes.last().rotation
+                    placedModelNodes.last().rotation = Rotation(x = currentRotation.x, y = currentRotation.y + rotationDelta, z = currentRotation.z)
+                    lastRotation = event.x
+                }
+            }
+        }
+    }
+
+    private fun loadAndPlaceModel(modelFileLocation: String): ArModelNode {
+        val offset = 1.5f * placedModelNodes.size // Adjust this offset based on how many models have been placed
+        val modelNode = ArModelNode(sceneView.engine, PlacementMode.INSTANT).apply {
             loadModelGlbAsync(
-                glbFileLocation = "models/sofa.glb",
+                glbFileLocation = modelFileLocation,
                 scaleToUnits = 1f,
-                centerOrigin = Position(-0.5f)
-
-            )
-            {
+                centerOrigin = Position(-0.5f + offset, 0f, -offset)
+            ) {
                 sceneView.planeRenderer.isVisible = true
-                val materialInstance = it.materialInstances[0]
             }
-            onAnchorChanged = {
-                placeButton.isGone = it != null
-            }
-
         }
         sceneView.addChild(modelNode)
-        modelNode.addChild(videoNode)
-
+        return modelNode
     }
 
-   private fun placeModel(){
-       modelNode.anchor()
-
-       sceneView.planeRenderer.isVisible = false
-
-   }
-
-    override fun onPause() {
-        super.onPause()
-        mediaPlayer.stop()
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer.release()
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+    // Override onPause and onDestroy if needed...
 }
